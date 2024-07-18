18069de08839586e47866b162f7ec53e19db4f21
Tristan Daniël Maat <t.maat@famedly.com>
2024-07-18 10:52:31 +0200

    feat!: Allow crate error to implement `Sync`

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use thiserror::Error;
+use zitadel::credentials::ServiceAccountError;
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -7,7 +8,7 @@ pub enum Error {
 	#[error("Operation timed out: {0}")]
 	TimoutError(#[from] tokio::time::error::Elapsed),
 	#[error("Zitadel service account error: {0}")]
-	ZitadelServiceAccountError(#[from] zitadel::credentials::ServiceAccountError),
+	ZitadelServiceAccountError(String),
 	#[error("Url parsing error (url crate): {0}")]
 	UrlParseError(#[from] url::ParseError),
 	#[error("Url parsing error (hyper/http crate): {0}")]
@@ -18,6 +19,17 @@ pub enum Error {
 	TonicTransportError(#[from] tonic::transport::Error),
 	#[error("Invalid tonic metadata value: {0}")]
 	InvalidMetadataValue(#[from] tonic::metadata::errors::InvalidMetadataValue),
+}
+
+// The `ServiceAccountError` has one error which is exposed as a
+// `Box<dyn Error>`, and therefore doesn't implement `Sync` - to allow
+// our error to be used in async-heavy crates, that therefore means we
+// need to not embed the underlying error, and implement our own
+// `From` impl without `thiserror`.
+impl From<ServiceAccountError> for Error {
+	fn from(error: ServiceAccountError) -> Self {
+		Self::ZitadelServiceAccountError(error.to_string())
+	}
 }
 
 pub type Result<R> = std::result::Result<R, Error>;
